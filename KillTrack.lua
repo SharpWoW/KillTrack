653c6265824adb07f05346750eb7618723711bc8
Adam Hellberg <sharparam@sharparam.com>
2022-05-08 12:51:59 +0200

    Add tracking of when a mob was last killed
    Can be seen in a tooltip in the GUI list view, or when querying data
    about a mob via slash commands.
    
    Implements #19

--- KillTrack.lua
+++ KillTrack.lua
@@ -25,6 +25,7 @@ _G[NAME] = KT
 local UnitGUID = UnitGUID
 local UnitIsTapDenied = UnitIsTapDenied
 local CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
+local GetServerTime = GetServerTime
 
 local NO_NAME = "<No Name>"
 
@@ -50,6 +51,10 @@ KT.Session = {
 }
 KT.Messages = {
     Announce = "[KillTrack] Session Length: %s. Session Kills: %d. Kills Per Minute: %.2f."
+}
+
+KT.Defaults = {
+    DateTimeFormat = "%Y-%m-%d %H:%M:%S"
 }
 
 local ET
@@ -167,6 +172,9 @@ function KT.Events.ADDON_LOADED(self, ...)
     end
     if type(self.Global.TOOLTIP) ~= "boolean" then
         self.Global.TOOLTIP = true
+    end
+    if type(self.Global.DATETIME_FORMAT) ~= "string" then
+        self.Global.DATETIME_FORMAT = self.Defaults.DateTimeFormat
     end
     if type(_G["KILLTRACK_CHAR"]) ~= "table" then
         _G["KILLTRACK_CHAR"] = {}
@@ -397,9 +405,14 @@ end
 
 function KT:AddKill(id, name)
     name = name or NO_NAME
+    local current_time = GetServerTime()
     self:InitMob(id, name)
-    self.Global.MOBS[id].Kills = self.Global.MOBS[id].Kills + 1
-    self.CharGlobal.MOBS[id].Kills = self.CharGlobal.MOBS[id].Kills + 1
+    local globalMob = self.Global.MOBS[id]
+    local charMob = self.CharGlobal.MOBS[id]
+    globalMob.Kills = self.Global.MOBS[id].Kills + 1
+    globalMob.LastKillAt = current_time
+    charMob.Kills = self.CharGlobal.MOBS[id].Kills + 1
+    charMob.LastKillAt = current_time
     if self.Global.PRINTKILLS then
         local kills = self.Global.MOBS[id].Kills
         local cKills = self.CharGlobal.MOBS[id].Kills
@@ -533,11 +546,15 @@ function KT:PrintKills(identifier)
     local name = NO_NAME
     local gKills = 0
     local cKills = 0
+    local lastKillAt = nil
     if type(identifier) ~= "string" and type(identifier) ~= "number" then identifier = NO_NAME end
     for k,v in pairs(self.Global.MOBS) do
         if type(v) == "table" and (tostring(k) == tostring(identifier) or v.Name == identifier) then
             name = v.Name
             gKills = v.Kills
+            if type(v.LastKillAt) == "number" then
+                lastKillAt = KTT:FormatDateTime(v.LastKillAt)
+            end
             if self.CharGlobal.MOBS[k] then
                 cKills = self.CharGlobal.MOBS[k].Kills
             end
@@ -546,6 +563,9 @@ function KT:PrintKills(identifier)
     end
     if found then
         self:Msg(("You have killed %q %d times in total, %d times on this character"):format(name, gKills, cKills))
+        if lastKillAt then
+            self:Msg(("Last killed at %s"):format(lastKillAt))
+        end
     else
         if UnitExists("target") and not UnitIsPlayer("target") then
             identifier = UnitName("target")
