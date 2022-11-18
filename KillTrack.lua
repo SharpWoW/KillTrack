678bcdd1e3c9d38d1564848450d29b6852871d60
Obsouleet <Obsouleet@users.noreply.github.com>
2022-11-18 13:44:06 +0100

    Update KillTrack.lua
    Gametooltip saw a lot changes in 10.0.2 - unfortunately this broke the addon.

--- KillTrack.lua
+++ KillTrack.lua
@@ -275,7 +275,7 @@ function KT.Events.ENCOUNTER_END(self, _, _, _, size)
     end
 end
 
-GameTooltip:HookScript("OnTooltipSetUnit", function(self)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit,function(self, data)
     if not KT.Global.TOOLTIP then return end
     local _, unit = self:GetUnit()
     if not unit then return end
